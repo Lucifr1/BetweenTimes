c6351318b07bf3113c1e97720e077411693bd4c8
Daniel <daniel.gottschalk@stud-mail.uni-wuerzburg.de>
2022-11-06 03:58:35 +0100

    jumppad slime animation

--- SpieleMarmelade/Assets/Scripts/Jumppad.cs
+++ SpieleMarmelade/Assets/Scripts/Jumppad.cs
@@ -6,9 +6,25 @@ using UnityEngine;
 public class Jumppad : MonoBehaviour
 {
    [SerializeField] private float jumppadPower;
+   private Animator jumppad;
+
+   private void Start()
+   {
+      jumppad = gameObject.transform.GetChild(0).GetComponent<Animator>();
+   }
 
    private void OnTriggerEnter2D(Collider2D other)
    {
+      jumppad.SetBool("boosting", true);
+      other.gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+      StartCoroutine(loadingBoost());
       other.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumppadPower, ForceMode2D.Impulse);
    }
+
+   IEnumerator loadingBoost()
+   {
+      yield return new WaitForSeconds(0.1f);
+      jumppad.SetBool("boosting", false);
+   }
+   
 }
